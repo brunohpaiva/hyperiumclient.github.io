f9421374b5dc8496988ca66c87c2910a832e0c7d
Bruno Henrique Paiva <bruno@sigane.com.br>
2018-10-29 10:24:15 -0300

    Improved 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,66 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 
 import Layout from '../components/Layout';
-import Container from '../components/Container';
+import Jumbotron from '../components/Jumbotron';
 
 const styles = theme => ({
-  item: theme.mixins.gutters(),
+  jumbotron: {
+    marginTop: '-24px',
+    [theme.breakpoints.down('xs')]: {
+      marginTop: '-32px',
+    },
+  },
+  pageTitle: {
+    fontWeight: 400,
+  },
 });
 
 const NotFoundPage = ({ classes, location }) => (
   <Layout location={location}>
-    <Container itemProps={{ className: classes.item }}>
-      <Typography variant="h2" gutterBottom>
-        NOT FOUND
+    <Jumbotron className={classes.jumbotron}>
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        gutterBottom
+        className={classes.pageTitle}
+      >
+        Not Found
       </Typography>
-      <Typography>
-        You just hit a route that doesn&#39;t exist... the sadness.
+      <Typography component="h2" variant="h6" align="center" gutterBottom>
+        {"You just hit a route that doesn't exist... the sadness."}
       </Typography>
-    </Container>
+      <Grid container spacing={16} justify="center">
+        <Grid item>
+          <Button variant="contained" color="primary" component={Link} to="/">
+            Home
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            component={Link}
+            to="/downloads"
+          >
+            Downloads
+          </Button>
+        </Grid>
+      </Grid>
+    </Jumbotron>
   </Layout>
 );
 
 NotFoundPage.propTypes = {
   classes: PropTypes.shape({
-    item: PropTypes.string.isRequired,
+    jumbotron: PropTypes.string.isRequired,
+    pageTitle: PropTypes.string.isRequired,
   }),
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
